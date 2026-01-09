99d7fc7f87f58eeb9ef14bc340e5fa3a07c167f8
1nto5 <aantosiak@icloud.com>
2026-01-09 15:55:43 +0100

    fix: add logging to deploy-watch.bat

--- deploy-watch.bat
+++ deploy-watch.bat
@@ -1,7 +1,6 @@
 @echo off
 :: Smart Home Auto-Deploy Watcher for Windows Task Scheduler
-:: Schedule this to run at system startup or periodically
-:: Task Scheduler: Run every 1-5 minutes, "Run whether user is logged on or not"
+:: Runs every 2 minutes via Task Scheduler
 
 cd /d C:\smart-home
-wsl -e bash -c "./deploy.sh"
+wsl -e bash -c "./deploy.sh >> deploy.log 2>&1"
